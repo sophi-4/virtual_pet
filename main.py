0686a6b11932bc84607e51388203a91b4fa086ee
Nick Rothwell <nick@cassiel.com>
2017-10-17 15:40:52 +0100

    Test deploy.

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
 from flask import Flask, request, render_template, jsonify
-from pymongo import MongoClient
 import datetime
 
 LOCAL = False
 
-from credentials import db_access
-
-app = Flask(__name__)
-
-host='ds033390.mlab.com:33390'
-db_name = 'test1'
-
-uri = "mongodb://%s:%s@%s/%s" % (db_access.user, db_access.password, host, db_name)
-
-print(uri)
-
-client = MongoClient(uri)
-db = client[db_name]
-
-for x in db.my_posts.find():
-    print(x)
-
 @app.route("/")
 def page():
-    return render_template('index.html')
+    return "Hello World"
 
 if LOCAL and __name__ == "__main__":
     app.run(debug=True)
