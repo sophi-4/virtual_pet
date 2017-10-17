dffa2948d8a7e5d811dc44ab8783d2336e8812f3
Nick Rothwell <nick@cassiel.com>
2017-10-17 15:56:13 +0100

    Templates.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def page():
-    return "Hello World"
+    return render_template('index.html')
 
 if LOCAL and __name__ == "__main__":
     app.run(debug=True)
