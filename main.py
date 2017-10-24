e84e8e16adbf4813a81906ce316d80f4f87e5c9b
Nick Rothwell <nick@cassiel.com>
2017-10-24 12:27:36 +0100

    DB data into template.

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def page():
-    return render_template('index.html')
+    return render_template('index.html', data=db.my_posts.find())
 
 if (not ON_HEROKU) and __name__ == "__main__":
     app.run(debug=True)
