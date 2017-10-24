6869917a54bf728f9a5dce7cb3b42c11c46a93fc
Nick Rothwell <nick@cassiel.com>
2017-10-24 11:11:48 +0100

    DB back in.

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from flask import Flask, request, render_template, jsonify
+from pymongo import MongoClient
 import datetime
 import os
+
+from credentials import db_access
+
+host='ds033390.mlab.com:33390'
+db_name = 'test1'
+
+uri = "mongodb://%s:%s@%s/%s" % (db_access.user, db_access.password, host, db_name)
+
+print(uri)
+
+client = MongoClient(uri)
+db = client[db_name]
+
+for x in db.my_posts.find():
+    print(x)
 
 ON_HEROKU = "ON_HEROKU" in os.environ
 
